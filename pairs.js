a7d886899c7c8b49a79dd004ea8207226b119a9b
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-09-03 15:04:15 -0300

    feat(java): resolve exercicio 43 - numeros pares de 1 a 100

--- pairs.js
+++ pairs.js
@@ -1,15 +1,17 @@
 const { createInterface } = require('node:readline/promises');
 const { stdin, stdout } = require('node:process');
+const console = require('node:console');
 
 async function main() {
-
     const rl = createInterface({ input: stdin, output: stdout });
 
-    for (let i = 100; i >= 1; i--) {
+    for (let i = 1; i <= 100; i++) {
 
-        console.log(i);
+        if (i % 2 == 0) {
+
+            console.log(i);
+        }
     }
     rl.close();
-
 }
 main();
