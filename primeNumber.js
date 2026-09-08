f7bc12a264e5837c11e6cc220c70632f9a61719c
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-09-08 17:07:58 -0300

    feat(js): resolve exercise 48 - primality test algorithm

---
+++ primeNumber.js
@@ -0,0 +1,29 @@
+const { createInterface } = require('node:readline/promises');
+const { stdin, stdout } = require('node:process');
+
+async function main() {
+
+    const rl = createInterface({ input: stdin, output: stdout });
+    const numero = Number(await rl.question("Digite um número: "));
+    let ehPrimo = true;
+    if (numero <= 1) {
+        ehPrimo = false;
+    }
+
+    for (let i = 2; i <= (numero - 1); i++) {
+
+        if (numero % i == 0) {
+            ehPrimo = false;
+            break;
+        }
+
+    }
+
+    if (ehPrimo) {
+        console.log(`O número é primo`);
+    } else {
+        console.log(`O número não é primo.`)
+    }
+rl.close();
+}
+main();
