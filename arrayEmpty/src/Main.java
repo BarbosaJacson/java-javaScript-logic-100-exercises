0719dafa792715703ce72bb195ac6fec140cf802
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-09-14 17:40:56 -0300

    feat(java): resolve exercise 61 - initialize and print 20-element array

---
+++ arrayEmpty/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] vetor = new int[20];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = 0;
+            System.out.println("Posição: "+i + ", " + vetor[i]);
+        }
+
+    }
+}
